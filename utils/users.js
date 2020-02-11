00e6fae0a20c320d75a19388f16ab9c7e2cc93f3
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-11 11:34:24 +0200

    Add management of users

--- utils/users.js
+++ utils/users.js
@@ -1,1 +1,50 @@
 const users = [];
+
+exports.addUser = ({ id, username, chatroom }) => {
+  username = username.trim().toLowerCase();
+  chatroom = chatroom.trim().toLowerCase();
+
+  // Validate Data
+  if (!username || !chatroom) {
+    return {
+      error: "Username and room are required"
+    };
+  }
+
+  // Check if user is exist
+  const existUser = users.find(user => {
+    const result = user.chatroom === chatroom && user.username === username;
+    return result;
+  });
+
+  // Validate Username
+  if (existUser) {
+    return {
+      error: "Username is in use"
+    };
+  }
+
+  // Store User
+  const user = { id, username, chatroom };
+  users.push(user);
+  return { user };
+};
+
+exports.removeUser = id => {
+  const index = users.findIndex(user => {
+    const result = user.id === id;
+    return result;
+  });
+
+  if (index != -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+exports.getUser = id => {
+  return users.find(user => user.id === id);
+};
+
+exports.getUsersInRoom = chatroom => {
+  return users.filter(user => user.chatroom === chatroom);
+}
